38603496ac0706aa5adaee1ee60068bf5e6669f0
Vadim <niras.sp@gmail.com>
2022-12-26 17:18:38 +0700

    Registration alpha 1.1

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from core.views import UserRegistrationView
 
 urlpatterns = [
-    path('signup/', UserRegistrationView.as_view(), name='signup'),
+    path('signup', UserRegistrationView.as_view(), name='signup'),
 
 ]
