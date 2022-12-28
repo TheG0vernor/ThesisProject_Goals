7230ec32f8c3ce17e9f47a51e32dd365fff6f6af
Vadim <niras.sp@gmail.com>
2022-12-28 09:46:40 +0700

    Registration alpha 1.4.1

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/core/', include('core.urls')),
+    # path('api/core/', include('core.urls')),
     path('core/', include('core.urls')),
     # path('oauth_path/', include('social_django.urls')),
 ]
