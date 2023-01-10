db9e6843a26164c1b3f542a26f34a320a1c2a09f
Vadim <niras.sp@gmail.com>
2023-01-11 06:16:20 +0700

    sharing board alpha 3.4

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -139,7 +139,6 @@ AUTHENTICATION_BACKENDS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    # 'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
